2df6ffe4a6e0aa947ef687de524b1359f6bcf04a
Tim Buchner <twometer@outlook.de>
2021-01-18 01:38:43 +0100

    Add ATA PIO defs and new IO functions

--- kernel/disk/atadisk.cpp
+++ kernel/disk/atadisk.cpp
@@ -1,13 +1,24 @@
+#include <kernel/io.h>
 #include <disk/atadisk.h>
 
 using namespace Kernel;
+
+#define ATA_PRIMARY_PORT 0x1F0
+#define ATA_PRIMARY_DEVICE_CONTROL_REG 0x3F6
+#define ATA_PRIMARY_ALTERNATE_STATUS_REG 0x376
+#define ATA_PRIMARY_IRQ 14
+
+#define ATA_SECONDARY_PORT 0x170
+#define ATA_SECONDARY_DEVICE_CONTROL_REG 0x3E6
+#define ATA_SECONDARY_ALTERNATE_STATUS_REG 0x366
+#define ATA_SECONDARY_IRQ 15
 
 namespace Disk
 {
 
     ATADisk::ATADisk(unsigned int disk_num)
     {
-        // Interrupts::AddHandler(?, this);
+        Interrupts::AddHandler(ATA_PRIMARY_IRQ, this);
     }
 
     void ATADisk::ReadBlock(uint64_t block_idx, uint64_t block_num, uint8_t *data)
--- kernel/include/kernel/io.h
+++ kernel/include/kernel/io.h
@@ -20,12 +20,40 @@ namespace Kernel
              * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
         }
 
+        static inline void Out16(uint16_t port, uint16_t val)
+        {
+            asm volatile("outw %0, %1" ::"a"(val), "d"(port));
+        }
+
+        static inline void Out32(uint32_t port, uint32_t val)
+        {
+            asm volatile("outl %0, %1" ::"a"(val), "d"(port));
+        }
+
         static inline uint8_t In8(uint16_t port)
         {
             uint8_t ret;
             asm volatile("inb %1, %0"
                          : "=a"(ret)
                          : "Nd"(port));
+            return ret;
+        }
+
+        static inline uint16_t In16(uint16_t port)
+        {
+            uint16_t ret;
+            asm volatile("inw %1, %0"
+                         : "=a"(ret)
+                         : "d"(port));
+            return ret;
+        }
+
+        static inline uint32_t In32(uint16_t port)
+        {
+            uint32_t ret;
+            asm volatile("inw %1, %0"
+                         : "=a"(ret)
+                         : "d"(port));
             return ret;
         }
     };
