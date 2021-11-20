6cb10ae9a1763e9f1cd276f5d8a5d099e9621f29
Twometer <twometer@outlook.de>
2021-11-20 02:34:32 +0100

    boot: Readd very basic boot sector

---
+++ boot/stage1.asm
@@ -0,0 +1,24 @@
+bits 16
+org 0x7C00
+
+boot:
+    ; Reset segment registers
+    xor ax, ax
+    mov ds, ax 
+    mov es, ax
+    
+    ; Make sure that we are at 0:0x7C00 and the BIOS didn't mess with us
+    jmp 0:init
+    init: equ $
+
+    ; Initialize a 4K stack
+    mov ax, 0x0900      
+    mov ss, ax
+    mov sp, 0x1000
+
+    ; Halt if we get here
+    cli
+    hlt
+
+times 510 - ($-$$) db 0 ; Pad to full boot sector
+dw 0xaa55               ; Boot signature
