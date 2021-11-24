66bd692aff62e070ad42ad41364f510e1f849444
Twometer <twometer@outlook.de>
2021-11-24 01:37:53 +0100

    meta: Buildscript importing sysroot files into image

--- scripts/gen-image.sh
+++ scripts/gen-image.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 build_dir="../build"
+sysroot_dir="../sysroot"
 image_file="$build_dir/nekosys.hdd"
 
 echo Creating file
@@ -11,6 +12,9 @@ parted -s $image_file mkpart primary 2048s 100%
 
 echo Formatting
 echfs-utils -m -p0 $image_file quick-format 512
+for filename in $(find $sysroot_dir -type f); do
+    echfs-utils -m -p0 $image_file import $filename ${filename#"$sysroot_dir"}
+done
 
 echo Copying bootloader
 dd if=$build_dir/boot/stage1 of=$image_file bs=16 count=27 conv=notrunc
